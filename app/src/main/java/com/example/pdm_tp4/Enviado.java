6a68669bbfa21496a3842c2ce4c8ab0c562bfc07
Matias Torres <matitorres87@gmail.com>
2019-09-16 23:31:17 -0300

    Initial commit

---
+++ app/src/main/java/com/example/pdm_tp4/Enviado.java
@@ -0,0 +1,22 @@
+package com.example.pdm_tp4;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Enviado extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_enviado, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/pdm_tp4/ui/send/SendFragment.java
@@ -0,0 +1,38 @@
+package com.example.pdm_tp4.ui.send;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
+
+import com.example.pdm_tp4.R;
+
+public class SendFragment extends Fragment {
+
+    private SendViewModel sendViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_send, container, false);
+
+        Button enviar = root.findViewById(R.id.btnEnviar);
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(getActivity(), R.id.nav_host_fragment).navigate(R.id.enviado);
+            }
+        });
+
+        return root;
+    }
+}
